b25ae3cc5636c6704d2ba87f3ec4a5f25f804547
Louet Eliot <louet.eliot@yahoo.fr>
2016-12-12 17:49:49 +0100

    Show new article only to signed users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 feature "Listing Articles" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
+    @john = User.create(email: "john@example.com", password: "password")
 
-    @article1 = Article.create(title: "The first article", body: "Lorem ipsum 1", user: john)
-    @article2 = Article.create(title: "The second article", body: "Lorem ipsum 2", user: john)
+    @article1 = Article.create(title: "The first article", body: "Lorem ipsum 1", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Lorem ipsum 2", user: @john)
   end
 
-  scenario "A user lists all articles" do
+  scenario "with articles created and user not signed in" do
     visit root_path
 
     expect(page).to have_content(@article1.title)
@@ -18,7 +18,24 @@ feature "Listing Articles" do
 
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
   end
+
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+
+    visit root_path
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+  end
+
 
   scenario "A user has no articles" do
     Article.delete_all
