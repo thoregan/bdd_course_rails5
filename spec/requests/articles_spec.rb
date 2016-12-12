afda57d471fc9a1e0021f9280c7960aaea6b2bb2
Louet Eliot <louet.eliot@yahoo.fr>
2016-12-12 19:00:17 +0100

    Restrict article delete to only owners

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -32,6 +32,17 @@ describe "Articles", type: :request do
         expect(flash[:alert]).to eq(flash_message)
       end
     end
+
+    context 'with signed in and owner user' do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "Handle request successfully" do
+        expect(response.status).to eq 200
+      end
+    end
   end
 
   describe 'GET /articles/:id' do
@@ -54,4 +65,43 @@ describe "Articles", type: :request do
       end
     end
   end
+
+  describe 'DELETE /articles/:id/edit' do
+    context 'with non-signed in user' do
+      before {delete "/articles/#{@article.id}"}
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+
+    context 'with signed in user who is non-owner' do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own article."
+
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+
+    context 'with signed in and owner user' do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "Handle request successfully" do
+        expect(response.status).to eq 302
+        expect(flash[:alert]).to eq(nil)
+      end
+    end
+  end
 end
+
