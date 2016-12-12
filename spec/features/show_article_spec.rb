4fdff2783db615dc39f8f97fa32be31eaa916d19
Louet Eliot <louet.eliot@yahoo.fr>
2016-12-12 17:30:33 +0100

    Fix broken show article spec

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,7 +2,10 @@ require "rails_helper"
 
 feature "show article" do
   before do
-    @article = Article.create(title: "A article title", body: "A cool article body")
+    john = User.create(email: "john@example.com", password: "password")
+    login_as(john)
+
+    @article = Article.create(title: "A article title", body: "A cool article body", user: john)
   end
 
   scenario "A user shows an article" do
